bea0581ab49aed221b1355e90186b7f16d1022a3
neru <120244424+naeeruu@users.noreply.github.com>
2023-01-30 14:03:30 +0700

    Update Presence.ts

--- types/Presence.ts
+++ types/Presence.ts
@@ -1,1 +1,17 @@
-export { ActivityType, PresenceStatus } from "../deps.ts";
+import { ActivityType, PresenceStatus } from "../deps.ts";
+
+export interface Activity {
+  name: string;
+  /** name of the activity you want to display */
+  type: ActivityType;
+  /** the type of activity you want to display */
+  url?: string
+  /** twitch or youtube stream url (required if the activity type is streaming */
+}
+
+export interface PresenceOptions {
+  activities: Activity[];
+  /** array of activity objects */
+  status: PresenceStatus;
+  /** status for presences */
+};
