9d670820995404392d4bc9a9405f33546822a1a6
neru <120244424+naeeruu@users.noreply.github.com>
2023-01-31 04:38:16 +0700

    Update Message.ts

--- src/events/Message.ts
+++ src/events/Message.ts
@@ -2,5 +2,5 @@ import { Message } from "../structures/Message.ts";
 import { Bot, DiscordMessage } from "../../deps.ts";
 
 export async function message(bot: Bot, message: DiscordMessage) {
-  return bot.client.emit("messageCreate", new Message(client, message));
+  return bot.client.emit("messageCreate", new Message(bot.client, message));
 }
