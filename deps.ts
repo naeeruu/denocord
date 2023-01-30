33b9a741a03e679a0bb1cc808488d2b05f409904
neru <120244424+naeeruu@users.noreply.github.com>
2023-01-30 17:21:02 +0700

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { ActivityTypes as ActivityType, createBot, GatewayIntents, startBot } from "https://deno.land/x/discordeno@18.0.1/mod.ts";
+export { ActivityTypes as ActivityType, createBot, GatewayIntents, PresenceStatus, startBot } from "https://deno.land/x/discordeno@18.0.1/mod.ts";
