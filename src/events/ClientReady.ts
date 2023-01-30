2254035ea11c6d336aa2efea426badcb5c8e4a99
neru <120244424+naeeruu@users.noreply.github.com>
2023-01-31 03:51:59 +0700

    Update ClientReady.ts

--- src/events/ClientReady.ts
+++ src/events/ClientReady.ts
@@ -1,4 +1,4 @@
-import { Bot, DiscordReady, User } from "../deps.ts";
+import { Bot, DiscordReady, User } from "../../deps.ts";
 
 export async function ready(bot: Bot, payload: { shardId: number; v: number; user: User; guilds: bigint[]; sessionId: string; shard?: number[]; applicationId: bigint; }, rawPayload: DiscordReady) {
   const { shardId, guilds, shard } = payload;
