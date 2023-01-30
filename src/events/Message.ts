ce1f2a3d2c7008a3ef6286f6d9236b9ffee93008
neru <120244424+naeeruu@users.noreply.github.com>
2023-01-31 06:52:04 +0700

    Update Message.ts

--- src/events/Message.ts
+++ src/events/Message.ts
@@ -1,6 +1,6 @@
 import { Message } from "../structures/Message.ts";
-import { Bot, RawMessage } from "../../deps.ts";
+import { Bot } from "../../deps.ts";
 
-export async function message(bot: Bot, message: Message) {
-  return bot.client.emit("messageCreate", (new Message(bot.client, message)));
+export async function messageCreate(bot: Bot, message: Message) {
+  return bot.client.emit("messageCreate", message);
 }
