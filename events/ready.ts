c97fc60b750aada8f1bff5579d3496f757a64af3
neru <120244424+naeeruu@users.noreply.github.com>
2023-01-30 14:19:40 +0700

    Delete events directory

--- events/ready.ts
+++
@@ -1,5 +0,0 @@
-import { Bot, DiscordReady, User } from "../deps.ts";
-
-export async function ready(bot: Bot, payload: { shardId: number; v: number; user: User; guilds: bigint[]; sessionId: string; shard?: number[]; applicationId: bigint; }, rawPayload: DiscordReady) {
-  return { shardId, shard };
-};
