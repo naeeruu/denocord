acd625f3bf2e533061ae2ef0a88c919c7578f801
neru <120244424+naeeruu@users.noreply.github.com>
2023-01-31 03:36:52 +0700

    Update ClientReady.ts

--- events/ClientReady.ts
+++ events/ClientReady.ts
@@ -1,8 +1,6 @@
 import { Bot, DiscordReady, User } from "../deps.ts";
 
 export async function ready(bot: Bot, payload: { shardId: number; v: number; user: User; guilds: bigint[]; sessionId: string; shard?: number[]; applicationId: bigint; }, rawPayload: DiscordReady) {
-  if (payload.shardId === bot.gateway.lastShardId) {
-    bot.client.available = true;
-    return bot.client.emit("ready", bot.client);
-  }
+  const { shardId, guilds, shard } = payload;
+  return bot.client.emit("ready", ({ client: bot.client, shardId, guilds, shard }));
 };
