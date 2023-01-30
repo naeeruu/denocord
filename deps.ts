9b6abec60fdd2e73cd180e7ab4aa8248e21e53ea
neru <120244424+naeeruu@users.noreply.github.com>
2023-01-30 18:20:30 +0700

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,1 +1,2 @@
+export { default as EventEmitter } from "https://deno.land/x/events@v1.0.0/mod.ts";
 export { ActivityTypes as ActivityType, createBot, GatewayIntents, PresenceStatus, startBot } from "https://deno.land/x/discordeno@18.0.1/mod.ts";
