15b5dd2bfea584e469b3a68dd38b7cb393ae6413
neru <120244424+naeeruu@users.noreply.github.com>
2023-01-31 04:11:53 +0700

    Update Message.ts

--- src/structures/Message.ts
+++ src/structures/Message.ts
@@ -1,7 +1,14 @@
+import { Client } from "./Client.ts";
 import { DiscordMessage } from "../../deps.ts";
 
 export class Message {
-  constructor(options: MessageOptions) {
-
+  constructor(client: Client, message: DiscordMessage) {
+    this.client = client;
+    this.id = `${message.id}`.replace("n", "");
+    this.author = message.author;
+    this.embeds = message.embeds;
+    this.components = message.components;
+    this.reference = message.reference_message;
+    this.tts = message.tts;
   }
 }
