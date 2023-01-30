6c9d7e6e82c4476c31380b3d038d3b41db2103bc
neru <120244424+naeeruu@users.noreply.github.com>
2023-01-30 12:12:13 +0700

    Update ImageOptions.ts

--- types/ImageOptions.ts
+++ types/ImageOptions.ts
@@ -1,2 +1,8 @@
+/** https://discord.com/developers/docs/reference#image-formatting */
 export type ImageSize = 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 2048 | 4096;
-export type ImageFormat = "jpeg" | "png" | "gif" | "webp";
+
+/** 
+* https://discord.com/developers/docs/reference#image-formatting
+* type json can only be used on stickers
+*/
+export type ImageFormat = "jpeg" | "jpg" | "json" | "png" | "gif" | "webp";
