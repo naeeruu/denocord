1b53566dee1a07f19100958bb2c0e3db393cf7a8
alya <118826675+ylaafx@users.noreply.github.com>
2023-02-01 04:12:46 +0700

    added base manager for client

--- src/structures/Manager.ts
+++ src/structures/Manager.ts
@@ -1,7 +1,17 @@
 import { Client } from "./Client.ts";
+export { Collection } from "../../deps.ts";
 
 export class BaseManager {
   constructor(client: Client) {
     this.client = client;
+    this.cache = Collection<any, any>;
+  }
+
+  get cache(): Collection<any, any> {
+    return this.cache;
+  }
+
+  get client(): Client {
+    return this.client;
   }
 }
