e0613202a29517892c3a5690489dedc550892aa4
alya <118826675+ylaafx@users.noreply.github.com>
2023-01-31 16:03:43 +0700

    Update Embed.ts

--- src/structures/Embed.ts
+++ src/structures/Embed.ts
@@ -1,8 +1,28 @@
-import { EmbedData } from "../types/Embed.ts";
-import { DiscordMessage } from "../../deps.ts";
+import { EmbedData, EmbedAuthor } from "../types/Embed.ts";
+import { DiscordEmbed } from "../../deps.ts";
 
 export class Embed {
   constructor(data: EmbedData) {
+    this.author = data.author;
+    this.color = data.color;
+    this.description = data.description;
+    this.fields = data.fields;
+    this.footer = data.footer;
+    this.title = data.title;
+    this.timestamp = data.timestamp;
+  }
 
+  setColor(color: number): Embed {
+    this.color = color;
+    return this;
+  }
+
+  setAuthor(author: EmbedAuthor): Embed {
+    const { name, iconURL, proxyURL, url } = author;
+    return this.author = { name, iconURL, proxyURL, url };
+  }
+
+  setDescription(description: string): Embed {
+    this.description = description;
   }
 }
