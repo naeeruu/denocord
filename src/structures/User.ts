87c45e883f11449d56e53ada9e0769d8c256daf8
neru <120244424+naeeruu@users.noreply.github.com>
2023-01-31 07:17:34 +0700

    Update User.ts

--- src/structures/User.ts
+++ src/structures/User.ts
@@ -9,10 +9,14 @@ export class User {
     this.client = client;
     /** The flags that exist in the user account */
     this.flags = user.flags;
+    /** The user's id */
+    this.id = user.id;
     /** User selected language */
     this.locale = user.locale;
     /** The type of Nitro subscription on a user's account */
     this.premiumType = user.premium_types;
+    /** The public flags on a user's account */
+    this.publicFlags = user.public_flags;
     /** account's owner username */
     this.username = user.username;
   }
