ad2852a6e3c123154f1f1afde197f5ef00b96b30
neru <120244424+naeeruu@users.noreply.github.com>
2023-01-31 18:28:31 +0700

    Update Embed.ts

--- src/structures/Embed.ts
+++ src/structures/Embed.ts
@@ -10,50 +10,72 @@ export class EmbedBuilder {
     this.footer = data.footer;
     this.title = data.title;
     this.timestamp = data.timestamp;
+    this.url = data.url;
   }
 
+  /** Sets the color of this embed */
   setColor(color: number): EmbedBuilder {
     this.color = color;
     return this;
   }
 
+  /** Sets the author of this embed */
   setAuthor(author: EmbedAuthor): EmbedBuilder {
     const { name, iconURL, proxyURL, url } = author;
     this.author = { name, iconURL, proxyURL, url };
     return this;
   }
 
+  /** Sets the description of this embed */
   setDescription(description: string): EmbedBuilder {
     this.description = description;
   }
 
+  /** Sets the title of this embed */
   setTitle(title: string): EmbedBuilder {
     this.title = title;
     return this;
   }
 
+  /** Add the embed's fields */
   addField(field: EmbedField): EmbedBuilder {
     this.fields.push(field);
     return this;
   }
 
+  /** Add some embed fields */
   addFields(fields: EmbedField[]): EmbedBuilder {
     this.fields.concat(fields);
     return this;
   }
 
+  /** Sets the embed's fields */
   setFields(fields: EmbedField[]): EmbedBuilder {
     this.fields = fields;
     return this;
   }
 
+  /** Sets the footer of this embed */
   setFooter(footer: EmbedFooter): EmbedBuilder {
     this.footer = footer;
     return this;
   }
 
+  /** Sets the timestamp of this embed */
   setTimestamp(timestamp: number): EmbedBuilder {
     this.timestamp = timestamp;
+    return this;
+  }
+
+  /** Sets the thumbnail of this embed */
+  setThumbnail(url: string): EmbedBuilder {
+    this.thumbnail = { url };
+    return this;
+  }
+
+  /** Sets the URL of this embed */
+  setURL(url: string): EmbedBuilder {
+    this.url = url;
     return this;
   }
 }
