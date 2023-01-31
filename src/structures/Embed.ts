9920622c14a86f815bf70809d52130bbf70bf1cd
alya <118826675+ylaafx@users.noreply.github.com>
2023-01-31 16:19:56 +0700

    Update Embed.ts

--- src/structures/Embed.ts
+++ src/structures/Embed.ts
@@ -1,4 +1,4 @@
-import { EmbedData, EmbedAuthor } from "../types/Embed.ts";
+import { EmbedData, EmbedAuthor, EmbedField } from "../types/Embed.ts";
 import { DiscordEmbed } from "../../deps.ts";
 
 export class Embed {
@@ -19,10 +19,41 @@ export class Embed {
 
   setAuthor(author: EmbedAuthor): Embed {
     const { name, iconURL, proxyURL, url } = author;
-    return this.author = { name, iconURL, proxyURL, url };
+    this.author = { name, iconURL, proxyURL, url };
+    return this;
   }
 
   setDescription(description: string): Embed {
     this.description = description;
+  }
+
+  setTitle(title: string): Embed {
+    this.title = title;
+    return this;
+  }
+
+  addField(field: EmbedField): Embed {
+    this.fields.push(field);
+    return this;
+  }
+
+  addFields(fields: EmbedField[]): Embed {
+    this.fields.concat(fields);
+    return this;
+  }
+
+  setFields(fields: EmbedField[]): Embed {
+    this.fields = fields;
+    return this;
+  }
+
+  setFooter(footer: EmbedFooter): Embed {
+    this.footer = footer;
+    return this;
+  }
+
+  setTimestamp(timestamp: number): Embed {
+    this.timestamp = timestamp;
+    return this;
   }
 }
