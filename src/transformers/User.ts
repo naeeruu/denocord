bde82a87e96b7d853768f32a4014e6bf112f247c
neru <120244424+naeeruu@users.noreply.github.com>
2023-01-31 12:51:39 +0700

    Update User.ts

--- src/transformers/User.ts
+++ src/transformers/User.ts
@@ -1,6 +1,6 @@
 import { User } from "../structures/User.ts";
 import { Bot, DiscordUser } from "../../deps.ts";
 
-export async function user(bot: Bot, user: DiscordUser) {
+export async function user(bot: Bot, user: DiscordUser): User {
   return new User(bot.client, user);
 }
