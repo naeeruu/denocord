42abcc33688655c81bc3a18749a9c702e892f0b1
neru <120244424+naeeruu@users.noreply.github.com>
2023-01-31 10:39:24 +0700

    Update ImageOptions.ts

--- src/types/ImageOptions.ts
+++ src/types/ImageOptions.ts
@@ -8,8 +8,6 @@ export type ImageSize = 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 2048 | 4096;
 export type ImageFormat = "jpeg" | "jpg" | "json" | "png" | "gif" | "webp";
 
 export interface ImageOptions {
-  /** whether the image is animated or not */
-  forceStatic?: boolean;
   /** image format for image generation */
   format?: ImageFormat;
   /** the size of the image you want to generate */
