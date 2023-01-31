5e3cc9cdad3ec4fa2ffc90395997551088b07508
alya <118826675+ylaafx@users.noreply.github.com>
2023-02-01 04:18:18 +0700

    added GuildManager and some function in it

--- src/structures/Manager.ts
+++ src/structures/Manager.ts
@@ -1,10 +1,9 @@
-import { Client } from "./Client.ts";
+import { Client, Guild } from "./mod.ts";
 export { Collection } from "../../deps.ts";
 
 export class BaseManager {
   constructor(client: Client) {
     this.client = client;
-    this.cache = Collection<any, any>;
   }
 
   get cache(): Collection<any, any> {
@@ -13,5 +12,17 @@ export class BaseManager {
 
   get client(): Client {
     return this.client;
+  }
+}
+
+export class GuildManager extends BaseManager {
+  constructor(client: Client) {
+    super(client);
+    this.cache = Collection<string, Guild>;
+  }
+
+  leave(guildId: string): Promise<void> {
+    const cache = this.cache.get(guildId);
+    if (cache) return await bot.helpers.leaveGuild(guildId);
   }
 }
