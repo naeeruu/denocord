7cc2daa7653ebe51de7a5570ceb02f436dab2fe2
neru <120244424+naeeruu@users.noreply.github.com>
2023-01-31 12:51:24 +0700

    Update Message.ts

--- src/transformers/Message.ts
+++ src/transformers/Message.ts
@@ -1,6 +1,6 @@
 import { Message } from "../structures/Message.ts";
 import { Bot, DiscordMessage } from "../../deps.ts";
 
-export async function message(bot: Bot, message: DiscordMessage) {
+export async function message(bot: Bot, message: DiscordMessage): Message {
   return new Message(bot.client, message);
 }
