f24bfab314c56cd011098ad752780818ebdd4536
ゾクル <111561391+zokuru@users.noreply.github.com>
2023-02-02 04:45:12 +0700

    Update Image.ts

--- src/types/Image.ts
+++ src/types/Image.ts
@@ -1,11 +1,4 @@
-/** https://discord.com/developers/docs/reference#image-formatting */
-export type ImageSize = 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 2048 | 4096;
-
-/** 
-* https://discord.com/developers/docs/reference#image-formatting
-* type json can only be used on stickers
-*/
-export type ImageFormat = "jpeg" | "jpg" | "json" | "png" | "gif" | "webp";
+export { ImageFormat, ImageSize } from "../../deps.ts";
 
 export interface ImageOptions {
   /** image format for image generation */
